faae506d9d4b79924e70f76682665b4cf3abb8ee
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-02 23:56:11 +0530

    Time: 3 ms (50.63%), Space: 41.7 MB (51.64%) - LeetHub

---
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int diff1 = nums[j] - nums[i];
+                if (diff1 == diff) {
+                    for (int k = j + 1; k < nums.length; k++) {
+                        if (nums[k] - nums[j] == diff) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
