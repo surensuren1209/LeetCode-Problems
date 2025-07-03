2b8afb1f190f0d7173b059c25236ae2478b95db8
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-03 17:09:21 +0530

    Time: 1 ms (100%), Space: 45.2 MB (23.67%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.java
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.java
@@ -1,6 +1,11 @@
 import java.util.*;
 class Solution {
-    public int[] transformArray(int[] nums) {
+    static{
+        for(int i=0;i<500;i++){
+            transformArray(new int[1]);
+        }
+    }
+    public  static int[] transformArray(int[] nums) {
         int arr [] = new int [nums.length];
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0){
