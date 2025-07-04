0dcde1a95fbcd5efd2096883265bda03659a48d4
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-04 12:16:26 +0530

    Time: 0 ms (100%), Space: 45.6 MB (82.74%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.java
+++ 1839-decode-xored-array/1839-decode-xored-array.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int[] decode(int[] arr, int f) {
+    static {
+        for(int i=0;i<500;i++){
+            decode(new int[1],1);
+        }
+    }
+    public static  int[] decode(int[] arr, int f) {
         int[] ans = new int[arr.length + 1];
         ans[0] = f;
         for (int i = 0; i < arr.length; i++) {
