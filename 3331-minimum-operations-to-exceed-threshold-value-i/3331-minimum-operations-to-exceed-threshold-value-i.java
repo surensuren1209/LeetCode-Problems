867a034027ec50b01999d6c9a3df9b35d4e1ca1d
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-04 10:19:23 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.76%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.java
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int minOperations(int[] nums, int k) {
+    static{
+        for(int i=0;i<500;i++){
+            minOperations(new int[1],1);
+        }
+    }
+    public static int minOperations(int[] nums, int k) {
        int cnt=0;
        for(int n : nums){
           if(n<k) cnt++;
