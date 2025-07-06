2054a7a38f92c5003bee1318aa697a249cf602cf
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-06 21:13:54 +0530

    Time: 0 ms (100%), Space: 45.1 MB (48.88%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int minOperations(int[] nums) {
+    static{
+        for(int i=0;i<500;i++){
+            minOperations(new int[1]);
+        }
+    }
+    public static int minOperations(int[] nums) {
         int op = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] <= nums[i - 1]) {
