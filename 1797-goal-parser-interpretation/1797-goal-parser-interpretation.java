d46291e918491c7ba6914fbf40a4391802b4ec32
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-19 14:03:03 +0530

    Time: 1 ms (60.82%), Space: 41.8 MB (27.2%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,10 +1,5 @@
 class Solution {
-    static{
-        for(int i=0;i<500;i++){
-            interpret(" ");
-        }
-    }
-    public  static String interpret(String str) {
+    public String interpret(String str) {
         return str.replace("()","o").replace("(","").replace(")",""); 
     }
 }
