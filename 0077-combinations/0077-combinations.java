ec2ffc29472ad821338f45fdd7afaaf8fff1e622
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-07-27 19:47:28 +0530

    Time: 18 ms (78.83%), Space: 94.7 MB (58.87%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(1, n, k, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void helper(int start, int n, int k, List<Integer> path, List<List<Integer>> result) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            path.add(i);
+            helper(i + 1, n, k, path, result);
+            path.remove(path.size() - 1); 
+        }
+    }
+}
