c4190871b76907df1bcb597e11e3effe514ef438
SURENDHAR <86002123+surensuren1209@users.noreply.github.com>
2025-09-13 10:29:23 +0530

    Time: 20 ms (5.69%), Space: 45.8 MB (5.45%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,19 +1,25 @@
+import java.util.regex.*;
+
 class Solution {
-    static{
-        for(int i=0;i<500;i++){
-            maxFreqSum(" ");
+    public int maxFreqSum(String s) {
+        int v = 0;
+        int co = 0;
+        String vowels = "aeiou";
+        
+        for (char c = 'a'; c <= 'z'; c++) {
+            String cr = String.valueOf(c);
+            Pattern p = Pattern.compile(Pattern.quote(cr));
+            Matcher m = p.matcher(s);
+            int count = 0;
+            while (m.find()) {
+                count++;
+            }
+            if (vowels.indexOf(c) != -1) {
+                v = Math.max(v, count);
+            } else {
+                co = Math.max(co, count);
+            }
         }
-    }
-    public  static int maxFreqSum(String s) {
-        s = s.toLowerCase();
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) if (Character.isLetter(c)) freq[c - 'a']++;
-        String v = "aeiou";
-        int maxV = 0, maxC = 0;
-        for (int i = 0; i < 26; i++) {
-            if (v.indexOf(i + 'a') >= 0) maxV = Math.max(maxV, freq[i]);
-            else maxC = Math.max(maxC, freq[i]);
-        }
-        return maxV + maxC;
+        return v + co;
     }
 }
